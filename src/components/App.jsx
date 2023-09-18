c0d5c6fa628f23aa6c1dee952cc6fb4d7917b73c
vladkasyan <voadkasyan.2006@gmail.com>
2023-09-18 16:54:00 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,6 @@ import { Contacts } from './contacts/contacts';
 import { Filter } from './filter/filter';
 
 export const App = () => {
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: ''
-  // };
   const savedcontacts = JSON.parse(localStorage.getItem('contacts'));
   const [filter, setFilter] = useState('');
   const [contacts, setContacts] = useState(() => savedcontacts ?? []);
@@ -58,7 +49,7 @@ export const App = () => {
   };
 
   const removeContact = contactId => {
-    setContacts(contacts.filter(({ id }) => id !== contactId));
+    setContacts(prevState => prevState.filter(({ id }) => id !== contactId));
   };
   return (
     <div>
--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -3,20 +3,25 @@ import PropTypes from 'prop-types';
 import { List, Listitem, Listbutton } from './contacts.module';
 
 export const Contacts = ({ contacts, onRemove }) => (
-    <List>
-      {contacts.map(contact => (
-        <Listitem key={contact.id} >
-          {contact.name + ' : ' + contact.number}
-          {<Listbutton type="button" name="delete" onClick={() => onRemove(contact.id)}>
-              delete
-            </Listbutton>}
-        </Listitem>
-      ))}
-    </List>
-  );
-  
-  Contacts.propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onRemove: PropTypes.func.isRequired,
-  };
-  
+  <List>
+    {contacts.map(contact => (
+      <Listitem key={contact.id}>
+        {contact.name + ' : ' + contact.number}
+        {
+          <Listbutton
+            type="button"
+            name="delete"
+            onClick={() => onRemove(contact.id)}
+          >
+            delete
+          </Listbutton>
+        }
+      </Listitem>
+    ))}
+  </List>
+);
+
+Contacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onRemove: PropTypes.func.isRequired,
+};
